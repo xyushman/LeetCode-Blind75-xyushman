93b8712c38e2ff7a03ce41112d074c185153689d
Ayushman <166736658+xyushman@users.noreply.github.com>
2026-01-12 23:47:18 +0530

    Time: 24 ms (33.77%), Space: 17.4 MB (82.35%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -2,26 +2,32 @@ class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
         int n = nums.size();
-        set<vector<int>> st;
 
-        for(int i =0; i<n; i++){
+        vector<vector<int>> ans;
+        sort(nums.begin(),nums.end());
+
+        for(int i=0; i<n; i++){
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+
             for(int j=i+1; j<n; j++){
-                unordered_set<long long> seen;
-                for(int k=j+1; k<n; k++){
-                    long long sum = nums[i] + nums[j];
-                    sum+=nums[k];
-                    long long r = target - sum;
+                if(j>i+1 && nums[j]==nums[j-1]) continue;
 
-                    if(seen.find(r)!= seen.end()){
-                        vector<int> temp = {nums[i],nums[j], nums[k], (int)r};
-                        sort(temp.begin(),temp.end());
-                        st.insert(temp);
+                int l = j + 1, r = n - 1;
+                while(l < r){
+                    long long sum = (long long)nums[i] + nums[j] + nums[l] + nums[r];
+
+                    if(sum == target){
+                        ans.push_back({nums[i], nums[j], nums[l], nums[r]});
+                        while(l < r && nums[l] == nums[l + 1]) l++;
+                        while(l < r && nums[r] == nums[r - 1]) r--;
+                        l++;
+                        r--;
                     }
-                    seen.insert(nums[k]);
+                    else if(sum < target) l++;
+                    else r--;
                 }
-
             }
         }
-        return vector<vector<int>> (st.begin(),st.end());
+        return ans;
     }
 };
