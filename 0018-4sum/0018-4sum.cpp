adaf764213436bcf34d5af0647e37bff4496c382
Ayushman <166736658+xyushman@users.noreply.github.com>
2026-01-12 22:59:44 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -6,9 +6,10 @@ public:
 
         for(int i =0; i<n; i++){
             for(int j=i+1; j<n; j++){
-                unordered_set<int> seen;
+                unordered_set<long long> seen;
                 for(int k=j+1; k<n; k++){
-                    long long  r = (long long)target - (nums[i]+nums[j]+nums[k]);
+                    long long sum = nums[i]+nums[j]+nums[k];
+                    long long  r = (long long)target - (sum);
 
                     if(seen.find(r)!= seen.end()){
                         vector<int> temp = {nums[i],nums[j], nums[k], (int)r};
