c168a5eba0e563c3427abe6d3e188cb9fd53b7bf
Ayushman <166736658+xyushman@users.noreply.github.com>
2026-01-12 22:58:07 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0018-4sum/0018-4sum.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        int n = nums.size();
+        set<vector<int>> st;
+
+        for(int i =0; i<n; i++){
+            for(int j=i+1; j<n; j++){
+                unordered_set<int> seen;
+                for(int k=j+1; k<n; k++){
+                    long long  r = (long long)target - (nums[i]+nums[j]+nums[k]);
+
+                    if(seen.find(r)!= seen.end()){
+                        vector<int> temp = {nums[i],nums[j], nums[k], (int)r};
+                        sort(temp.begin(),temp.end());
+                        st.insert(temp);
+                    }
+                    seen.insert(nums[k]);
+                }
+
+            }
+        }
+        return vector<vector<int>> (st.begin(),st.end());
+    }
+};
