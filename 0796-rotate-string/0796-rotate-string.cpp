c23c4abb822630d8fd544ffbf938126f406aa25d
Ayushman <166736658+xyushman@users.noreply.github.com>
2026-01-28 22:37:14 +0530

    Time: 0 ms (100%), Space: 8.3 MB (25.42%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.cpp
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
     bool rotateString(string s, string goal) {
+
         if(s.size()!=goal.size()) return false;
 
-        for(int i=0; i<s.size(); i++){
-            string r = s.substr(i)+s.substr(0,i);
-            if(r==goal) return true;
-        }
-        return false;
+        string d = s+s;
+
+        return d.find(goal)!=string::npos;
     }
 };
