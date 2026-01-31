c660450b5d7f790c497d895f07a0ef7cb7db94bf
Ayushman <166736658+xyushman@users.noreply.github.com>
2026-01-31 15:49:50 +0530

    Time: 11 ms (63.42%), Space: 9.2 MB (91.67%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,33 +1,30 @@
 class Solution {
 public:
-    string longestPalindrome(string s) {
-        if (s.empty()) {
-            return "";
-        }
+        string longestPalindrome(string s) {
+        if (s.empty()) return "";
 
-        int start = 0;
-        int end = 0;
+        int start = 0, end = 0;
 
-        for (int i = 0; i < s.length(); i++) {
-            int odd = expandAroundCenter(s, i, i);
-            int even = expandAroundCenter(s, i, i + 1);
-            int max_len = max(odd, even);
+        // Lambda to expand around center
+        auto expand = [&](int left, int right) -> int {
+            while (left >= 0 && right < s.size() && s[left] == s[right]) {
+                left--;
+                right++;
+            }
+            return right - left - 1; // length of palindrome
+        };
 
-            if (max_len > end - start) {
-                start = i - (max_len - 1) / 2;
-                end = i + max_len / 2;
+        for (int i = 0; i < s.size(); i++) {
+            int len1 = expand(i, i);       // odd length palindrome
+            int len2 = expand(i, i + 1);   // even length palindrome
+            int maxlen = max(len1, len2);  // corrected variable name
+
+            if (maxlen > end - start) {    // use maxlen, not max_len
+                start = i - (maxlen - 1) / 2;
+                end = i + maxlen / 2;
             }
         }
 
-        return s.substr(start, end - start + 1);        
+        return s.substr(start, end - start + 1);
     }
-
-private:
-    int expandAroundCenter(string s, int left, int right) {
-        while (left >= 0 && right < s.length() && s[left] == s[right]) {
-            left--;
-            right++;
-        }
-        return right - left - 1;
-    }    
 };
