1340a33277cc9874680ab0359ac9c52410645f0d
Ayushman <166736658+xyushman@users.noreply.github.com>
2026-01-31 22:28:51 +0530

    Time: 2 ms (4.03%), Space: 8.6 MB (43.38%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    double cal(double x, long long n) {
+        if (n == 0) return 1.0;
+
+        double half = cal(x, n / 2);
+
+        if (n % 2 == 0)
+            return half * half;
+        else
+            return half * half * x;
+    }
+
+    double myPow(double x, int n) {
+        long long N = n;  // prevent overflow
+
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
+        }
+
+        return cal(x, N);
+    }
+};
