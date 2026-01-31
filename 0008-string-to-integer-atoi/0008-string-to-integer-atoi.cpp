b0eb78eab7bd80f3ea479ca8c5ee438d89a7b5e8
Ayushman <166736658+xyushman@users.noreply.github.com>
2026-01-31 18:17:54 +0530

    Time: 1 ms (22.84%), Space: 9.3 MB (21.73%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -1,34 +1,25 @@
 class Solution {
 public:
+
+    int helper(const string &s, int i, long long num, int sign){
+        if(i>=s.size() || !isdigit(s[i])) return (int)(sign*num);
+
+        num = num*10+(s[i]-'0');
+
+        if(sign*num <= INT_MIN) return INT_MIN;
+        if(sign*num >= INT_MAX) return INT_MAX;
+
+        return helper(s,i+1,num,sign);
+    }
     int myAtoi(string s) {
-        int i = 0, n = s.size();
+        int i=0;
+        while(i<s.size() && s[i]==' ') i++;
         int sign = 1;
-        int res = 0;
-
-        // 1. Skip leading whitespace
-        while (i < n && s[i] == ' ') i++;
-
-        // 2. Handle optional sign
-        if (i < n && (s[i] == '+' || s[i] == '-')) {
-            sign = (s[i] == '-') ? -1 : 1;
+        if(i<s.size() && (s[i]=='+' || s[i]=='-')) {
+            sign = (s[i] == '-') ? -1:1;
             i++;
         }
 
-        // 3. Convert digits
-        while (i < n && isdigit(s[i])) {
-            int digit = s[i] - '0';
-
-            // 4. Overflow check BEFORE multiplying
-            if (res > INT_MAX / 10 ||
-               (res == INT_MAX / 10 && digit > 7)) {
-                return sign == 1 ? INT_MAX : INT_MIN;
-            }
-
-            res = res * 10 + digit;
-            i++;
-        }
-
-        // 5. Apply sign
-        return res * sign;
+        return helper(s,i,0,sign);
     }
 };
