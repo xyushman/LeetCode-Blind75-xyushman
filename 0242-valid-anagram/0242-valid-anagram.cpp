d119b164cedae1fc9de934791562782995a0937d
Ayushman <166736658+xyushman@users.noreply.github.com>
2026-06-12 20:04:50 +0530

    Time: 3 ms (45.83%), Space: 9.5 MB (92.1%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,18 +1,8 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if(s.size() != t.size()) return false;
-
-        int freq[26] = {0};
-        for(int i=0; i<s.size(); i++){
-            freq[s[i]-'a']++;
-        }
-        for(int i=0; i < t.size(); i++){
-            freq[t[i]-'a']--;
-        }
-        for(int i=0; i<26; i++){
-            if(freq[i]!=0) return false;
-        }
-        return true;
+       sort(s.begin(),s.end());
+       sort(t.begin(),t.end());
+       return s==t; 
     }
 };
