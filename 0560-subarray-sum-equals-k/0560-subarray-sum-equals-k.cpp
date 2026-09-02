e456e6b3ce81e12c01903a3c4f5177a5c54c8cc8
Ayushman <166736658+xyushman@users.noreply.github.com>
2026-09-02 10:07:37 +0530

    Time: 1412 ms (20.66%), Space: 35.9 MB (80.5%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,33 +1,27 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        int n = nums.size();
-        int cnt = 0;
+        int cnt=0; 
 
+        int n = nums.size();
         vector<int> prefix(n);
 
-        prefix[0] = nums[0];
-
-        for (int i = 1; i < n; i++) {
-            prefix[i] = prefix[i - 1] + nums[i];
+        prefix[0]=nums[0];
+        for(int i=1;i<n; i++){
+            prefix[i] = prefix[i-1]+nums[i];
         }
 
-        // Subarrays starting from index 0
-        for (int i = 0; i < n; i++) {
-            if (prefix[i] == k) {
-                cnt++;
+        //  1 2 3
+        for(int i=0; i<n; i++){
+           
+            if(k==prefix[i]) cnt++;
+        }
+
+        for(int i=1; i<n; i++){
+            for(int j=0; j<i; j++){
+                if(prefix[i]-prefix[j]==k) cnt++;
             }
         }
-
-        // Subarrays starting from index > 0
-        for (int i = 1; i < n; i++) {
-            for (int j = 0; j < i; j++) {
-                if (prefix[i] - prefix[j] == k) {
-                    cnt++;
-                }
-            }
-        }
-
         return cnt;
     }
 };
