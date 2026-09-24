6757fe7963975e35d71c90aa2fcc3bf3068695c4
Ayushman <166736658+xyushman@users.noreply.github.com>
2026-09-24 15:24:35 +0530

    Time: 380 ms (25.45%), Space: 11.6 MB (89.66%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,14 +1,23 @@
 class Solution {
 public:
-    int solve(vector<int> &nums, int i,int sum, int tar){
-        if(i==nums.size()) return sum==tar;
+    int solve(vector<int> &nums, int i,int sum ){
+        if(i==nums.size()) return sum==0;
 
-        int take = solve(nums,i+1, sum+nums[i], tar);
+        int take = 0;
+        if(nums[i]<=sum) take = solve(nums,i+1, sum-nums[i]);
 
-        int ntake = solve(nums,i+1, sum-nums[i],tar);
+        int ntake = solve(nums,i+1, sum);
         return take+ntake;
     }
     int findTargetSumWays(vector<int>& nums, int target) {
-        return solve(nums,0,0,target);
+        int total = accumulate(nums.begin(),nums.end(),0);
+
+        if(target > total) return 0;
+
+        if((target+total)%2 != 0) return 0;
+
+        int req = (total+target)/2;
+
+        return solve(nums,0,req);
     }
 };
