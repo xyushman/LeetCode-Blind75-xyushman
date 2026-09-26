81d8528675d9f2d16c8ef5afa82f10bfeded3908
Ayushman <166736658+xyushman@users.noreply.github.com>
2026-09-26 20:55:05 +0530

    Time: 0 ms (100%), Space: 15 MB (54.74%) - LeetHub

--- 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.cpp
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.cpp
@@ -4,9 +4,15 @@ public:
         int n = mat.size();
         int sum =0;
 
-        for(int i=0; i<n; i++) sum+=mat[i][i];
+        int l =0, r = n-1;
 
-        for(int i=0; i<n; i++) sum+=mat[i][n-i-1];
+        for(int i=0; i<n; i++){
+            sum+=mat[i][l];
+            sum+=mat[i][r];
+
+            l++;
+            r--;
+        }
 
         if(n%2 == 1) sum-=mat[n/2][n/2];
 
